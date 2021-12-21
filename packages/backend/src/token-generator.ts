1c97e33a1978f32be81aa57f03ba30cef320e4ea
Grzegorz Tańczyk <fb@tanczyk.pl>
2021-12-21 19:13:18 +0100

    add console logs

--- packages/backend/src/token-generator.ts
+++ packages/backend/src/token-generator.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { DOLBY_CONSUMER_KEY, DOLBY_CONSUMER_SECRET } from "./config";
 
 export async function generateToken() {
+  console.log("Start generating token");
   const credentials = Buffer.from(
     DOLBY_CONSUMER_KEY + ":" + DOLBY_CONSUMER_SECRET
   ).toString("base64");
@@ -16,11 +17,13 @@ export async function generateToken() {
   const data = { grant_type: "client_credentials", expires_in: 3600 };
 
   try {
+    console.log("Make a request to voxeet.com");
     const response = await axios.post(url, data, config);
     const { access_token } = response.data;
+    console.log("Token generated");
     return access_token;
   } catch (e) {
-    console.error(e);
+    console.error("Failed to generate token", e);
     return null;
   }
 }
